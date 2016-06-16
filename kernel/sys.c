14837cb935ea9161e7ab49c228744e1b71414ef0
ShevT <shev_t@mail.ru>
2016-06-16 09:19:03 +0300

    kernel: sys: fix switch - case

--- kernel/sys.c
+++ kernel/sys.c
@@ -1967,6 +1967,7 @@ SYSCALL_DEFINE5(prctl, int, option, unsigned long, arg2, unsigned long, arg3,
 				tsk->timer_slack_ns = arg2;
 			put_task_struct(tsk);
 			error = 0;
+			break;
 		case PR_SET_VMA:
 			error = prctl_set_vma(arg2, arg3, arg4, arg5);
 			break;
