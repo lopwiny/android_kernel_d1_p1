ce7c32fd065dc2a54d9c24c9a2e0bb451ff42a14
ShevT <shev_t@mail.ru>
2016-12-07 12:33:32 +0300

    Revert "enable virtio_net to return bus_info in ethtool -i consistent with emulated NICs"
    This reverts commit 5fef30f607cce3d9b498e07d63f58e9bf02b4d17.

--- drivers/s390/kvm/kvm_virtio.c
+++ drivers/s390/kvm/kvm_virtio.c
@@ -262,11 +262,6 @@ error:
 	return PTR_ERR(vqs[i]);
 }
 
-static const char *kvm_bus_name(struct virtio_device *vdev)
-{
-	return "";
-}
-
 /*
  * The config ops structure as defined by virtio config
  */
@@ -280,7 +275,6 @@ static struct virtio_config_ops kvm_vq_configspace_ops = {
 	.reset = kvm_reset,
 	.find_vqs = kvm_find_vqs,
 	.del_vqs = kvm_del_vqs,
-	.bus_name = kvm_bus_name,
 };
 
 /*
