d1432b41da1484a78397afe9ff64a374a3402e77
ShevT <shev_t@mail.ru>
2017-01-03 14:50:41 +0300

    mfd: Do not reboot when switching off. Even if charging

--- drivers/mfd/twl6030-poweroff.c
+++ drivers/mfd/twl6030-poweroff.c
@@ -20,8 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-/*===========================================================================
-=============================================================================*/
+
 #include <linux/module.h>
 #include <linux/pm.h>
 #include <linux/i2c/twl.h>
@@ -30,50 +29,10 @@
 #define CON_DEVOFF	(1<<1)
 #define MOD_DEVOFF	(1<<2)
 
-#include <linux/reboot.h>
-#include <linux/syscalls.h>
-#define SW_RESET      (1<<6)
-#define CONTROLLER_STAT1		0x03
-#define	VBUS_DET   (1<<2)
-
-#define STS_HW_CONDITIONS		0x21
-#define STS_USB_ID			(1<<2)
-
-#define RUNMODE_FLAG_FACTORY 1
-#define RUNMODE_FLAG_NORMAL 0
-extern unsigned char runmode_is_factory(void);
-
-static int is_charger_present(void)
-{
-	u8 vbus_state=0, hw_state=0;
-	int charger_present =0;
-
-	twl_i2c_read_u8(TWL6030_MODULE_CHARGER, &vbus_state,CONTROLLER_STAT1);
-	twl_i2c_read_u8(TWL6030_MODULE_ID0, &hw_state, STS_HW_CONDITIONS);
-	vbus_state = vbus_state & VBUS_DET;
-    /*check charger present*/
-	if(vbus_state && !(hw_state & STS_USB_ID)){
-		charger_present = 1;
-	}
-	else{
-		charger_present = 0;
-	}
-	return charger_present;
-}
-
-
 void twl6030_poweroff(void)
 {
 	u8 val = 0;
 	int err = 0;
-       if(RUNMODE_FLAG_FACTORY != runmode_is_factory())
-	{
-	    if (is_charger_present()){
-		pr_info("%s: charger present poweroff reboot\n", __func__);
-		kernel_restart("charger_poweroff_reboot");
-		return;
-	    }
-       }
 
 	err = twl_i2c_read_u8(TWL_MODULE_PM_MASTER, &val,
 				  TWL6030_PHOENIX_DEV_ON);
